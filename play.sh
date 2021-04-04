639a9fcf5d3df8d2eb43f7907a3d015f9272e5e7
kaitoy <kaitoy@pcap4j.org>
2021-04-04 14:01:06 +0900

    Fix a typo in play.sh so ansible can load extra_vars.yml.

--- play.sh
+++ play.sh
@@ -6,4 +6,4 @@ export ANSIBLE_HOST_KEY_CHECKING=False
 export ANSIBLE_SSH_ARGS="-o ControlMaster=no -o ControlPersist=60s"
 export ANSIBLE_PIPELINING=True
 export ANSIBLE_TIMEOUT=1800
-ansible-playbook -vv -e extra_vars.yml -i $1 $2
+ansible-playbook -vv -e @extra_vars.yml -i $1 $2
